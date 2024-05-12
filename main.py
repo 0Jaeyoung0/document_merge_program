1e2786944b4462996ec52d0c1e722ca69cbe5011
sye <dpdms8938@naver.com>
2024-05-12 22:03:33 +0900

    ADD a GUI

--- main.py
+++ main.py
@@ -2,26 +2,51 @@ import src.file_io.file_io as file_io
 import src.convert_document.convert_to_pdf as convert_to_pdf
 import src.merge_document.merge_pdf as merge_pdf
 import os
-
+'index'
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Cm
 
-if __name__ == '__main__':
-    file_selector = file_io.FileIO()
-    to_pdf_converter = convert_to_pdf.ToPdfConverter()
-    pdf_merger = merge_pdf.PdfMerger()
+'gui'
+from tkinter import *
+import tkinter
+from tkinter.messagebox import *
+from tkinter import messagebox
+import os
 
+
+
+def btn_load_click(event):
+    global input_files
     input_files = file_selector.open_files()
-    converted_files = []
+    for file in input_files:
+        file_basename = os.path.basename(file)  # 파일 이름만 추출
+        listbox.insert(END, file_basename)  # 리스트박스에 항목 추가
+    return input_files
 
+def btn_delete_click(event):
+    selected_items = listbox.curselection()  # 선택된 항목의 인덱스를 가져옴
+    if askokcancel("Delete", f"Are you sure you want to delete it?"):
+        print("yes")
+        # 선택된 항목을 뒤에서부터 삭제하여 인덱스 변화를 방지
+        for i in reversed(selected_items):
+            listbox.delete(i)  # 리스트박스에서 항목 삭제
+            del input_files[i]  # 전역 리스트에서 항목 삭제
+
+    else:
+        print("no")
+
+def btn_merge_click(event, input_files):
+    print("merge")
+
+    converted_files = []
     for index, input_file in enumerate(input_files, start=1):
         file_path, file_name = os.path.split(input_file)
         converted_file = os.path.join(file_path, f'temp{index}.pdf')
 
         to_pdf_converter.convert_to_pdf(input_file=input_file, output_file=converted_file)
-        if(input_file.endswith('.doc')) or input_file.endswith('.docx'):
-            to_pdf_converter.extract_page(converted_file, input_file)
+        # if(input_file.endswith('.doc')) or input_file.endswith('.docx'):
+        #    to_pdf_converter.extract_page(converted_file, input_file)
 
         converted_files.append(converted_file)
 
@@ -31,7 +56,44 @@ if __name__ == '__main__':
 
     for converted_file in converted_files:
         os.remove(converted_file)
+    listbox.delete(0, END)
+    input_files = []
+    tkinter.messagebox.showinfo("Complete","Merged is complete.")
 
+
+if __name__ == '__main__':
+
+    file_selector = file_io.FileIO()
+    to_pdf_converter = convert_to_pdf.ToPdfConverter()
+    pdf_merger = merge_pdf.PdfMerger()
+
+    root = Tk()
+    root.title("N&M")  # 창 제목 설정
+    root.resizable(width=False, height=False)  # 창 크기 조절 불가능하도록 설정
+    root.geometry("400x200+100+100")
+
+    listbox = Listbox(root, width=20)  # 리스트박스 생성
+    listbox.pack()
+    # listbox.grid(row=0, column=0, columnspan=3, sticky='ew')  # 리스트박스 위치 조정
+    listbox.pack(side="top", fill="x")
+
+    # 버튼 생성 및 위치 조정
+    btn_load = Button(root, text='파일 로드', width=10)
+    btn_delete = Button(root, text='파일 삭제', width=10)
+    btn_merge = Button(root, text='파일 병합', width=10)
+    btn_load.pack(side="left")
+    btn_delete.pack(side="left")
+    btn_merge.pack(side="right")
+
+    # 이벤트 바인딩
+    btn_load.bind('<Button-1>', btn_load_click)
+    btn_delete.bind('<Button-1>', btn_delete_click)
+    btn_merge.bind('<Button-1>', lambda event: btn_merge_click(event, input_files))  # 이벤트 바인딩 시 input_files 전달
+
+    root.mainloop()
+
+'index'
+'''
     document = Document()
 
 
@@ -76,3 +138,4 @@ if __name__ == '__main__':
         row.cells[2].paragraphs[0].alignment = WD_ALIGN_PARAGRAPH.CENTER
         total_page_num = total_page_num+files_pages[i-1]
     document.save('test.docx')
+'''
